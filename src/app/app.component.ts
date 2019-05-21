7c4fff34c5990d4729118cf337832e1636e763f3
Nate C <N_t_N@live.com>
2019-05-21 16:20:56 +0800

    bulma added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import * as data from './data.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bluehead';
+  data = data;
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Bootstrap3FrameworkModule, Bootstrap4FrameworkModule, MaterialDesignFrameworkModule} from 'angular6-json-schema-form';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialDesignFrameworkModule,
+    Bootstrap3FrameworkModule,
+    Bootstrap4FrameworkModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
